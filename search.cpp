ba316f0d4e4981d51efc5872decaa625fb6e835b
Sujal Kapoor <kapoor.sujal777@gmail.com>
2025-03-16 11:30:19 +0530

    Add files via upload

--- search.cpp
+++ search.cpp
@@ -1,29 +1,24 @@
 #include "search.h"
-
 SearchEngine::SearchEngine()
 {
     this->Storage = new SentenceList();
 }
-
 SearchEngine::~SearchEngine()
 {
     delete this->Storage;
 }
-
 void ConvertToLowercase(string &sentence)
 {
     for (char &c : sentence)
         if (c <= 'Z' && c >= 'A')
             c = c - ('A' - 'a');
 }
-
 void SearchEngine::insert_sentence(int book_code, int page, int paragraph, int sentence_no, string sentence)
 {
     ConvertToLowercase(sentence);
     this->Storage->insert(sentence, book_code, page, paragraph, sentence_no);
     return;
 }
-
 Node *SearchEngine::search(string pattern, int &n_matches)
 {
     ConvertToLowercase(pattern);
